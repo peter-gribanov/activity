e3da186867401dfe13ae6883eee7f7bf7a7b9475
Peter Gribanov <info@peter-gribanov.ru>
2013-05-20 12:22:46 +0400

    add mothods for get row by primary key and fetch on or more rows

--- Framework/Database/Table.php
+++ Framework/Database/Table.php
@@ -10,28 +10,46 @@
 
 namespace Framework\Database;
 
+use Framework\Database\Engine;
+
 /**
  * Таблица базы данных
  *
  * @package Framework\Database
  * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
-class Table {
+abstract class Table {
 
 	/**
-	 * Имя таблици
+	 * Название таблицы
 	 *
 	 * @var string
 	 */
 	const TABLE_NAME = 'table';
 
 	/**
-	 * Главное поле
+	 * Ключевой столбец
 	 *
 	 * @var string
 	 */
-	const PRIMARY_COLUMN = 'id';
+	protected $column_primary = 'id';
 
+	/**
+	 * Адаптор доступа к бд
+	 *
+	 * @var \Framework\Database\Engine
+	 */
+	protected $engine;
+
+
+	/**
+	 * Конструктор
+	 *
+	 * @param \Framework\Database\Engine $engine  Адаптор доступа к бд
+	 */
+	public function __construct(Engine $engine) {
+		$this->engine = $engine;
+	}
 
 	/**
 	 * Возвращает имя таблици
@@ -42,4 +60,78 @@ class Table {
 		return static::TABLE_NAME;
 	}
 
+	/**
+	 * Возвращает первичный ключ
+	 *
+	 * @return string
+	 */
+	public function getPrimaryKey() {
+		return $this->column_primary;
+	}
+
+	/**
+	 * Выбирает поле по первичному ключу
+	 *
+	 * @param mixed $id Id
+	 *
+	 * @return \Framework\Database\Table\Row
+	 */
+	public function get($id) {
+		$st = $this->engine->prepare('
+			SELECT
+				*
+			FROM
+				`'.static::TABLE_NAME.'`
+			WHERE
+				:primary = :id
+			');
+		$st->bindValue(':primary', $this->column_primary, Engine::PARAM_ID);
+		$st->bindValue(':id', $id, Engine::PARAM_STR);
+		$st->execute();
+		return $st->fetch();
+	}
+
+	/**
+	 * Выбрать все записи
+	 *
+	 * @param string $where  Условие выбора
+	 * @param array  $params Параметры
+	 *
+	 * @return array
+	 */
+	public function fetchAll($where = 'true', array $params = array()) {
+		/* @var $st \Framework\Database\Statement */
+		$st = $this->engine->prepare('
+			SELECT
+				*
+			FROM
+				`'.static::TABLE_NAME.'`
+			WHERE
+				'.$where
+		);
+		$st->execute($params);
+		return $st->fetchAll();
+	}
+
+	/**
+	 * Выбрать одну запись
+	 *
+	 * @param string $where  Условие выбора
+	 * @param array  $params Параметры
+	 *
+	 * @return array
+	 */
+	public function fetchRow($where, array $params = array()) {
+		/* @var $st \Framework\Database\Statement */
+		$st = $this->engine->prepare('
+			SELECT
+				*
+			FROM
+				`'.static::TABLE_NAME.'`
+			WHERE
+				'.$where
+		);
+		$st->execute($params);
+		return $st->fetch();
+	}
 }
