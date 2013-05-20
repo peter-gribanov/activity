9d3bb1a6e61caa78122d9289439959551113bf8c
Peter Gribanov <info@peter-gribanov.ru>
2013-05-20 18:07:53 +0400

    wrap get method for return utm for start and end date

--- Framework/Model/Activity.php
+++ Framework/Model/Activity.php
@@ -75,4 +75,16 @@ class Activity extends ActivityTable {
 		return $result;
 	}
 
+	/**
+	 * (non-PHPdoc)
+	 * @see Framework\Database.Table::get()
+	 */
+	public function get($id) {
+		if ($item = parent::get($id)) {
+			$item['date_start'] = strtotime($item['date_start']);
+			$item['date_end'] = strtotime($item['date_end']);
+		}
+		return $item;
+	}
+
 }
