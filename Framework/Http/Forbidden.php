99aed6654debdcc23356faad528f73c4a2797040
Peter Gribanov <info@peter-gribanov.ru>
2013-05-20 12:16:37 +0400

    add forbidden http exception

---
+++ Framework/Http/Forbidden.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Framework package
+ *
+ * @package   Framework
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2012, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace Framework\Http;
+
+use Framework\Http\Http;
+use Framework\Http\Status;
+
+/**
+ * Исключение Forbidden
+ * 
+ * @package Framework\Http
+ * @author  Peter Gribanov <gribanov@professionali.ru>
+ */
+class Forbidden extends Http {
+
+	/**
+	 * Конструктор
+	 * 
+	 * @param string|null $message Сообщение
+	 */
+	public function __construct($message = '') {
+		parent::__construct($message, Status::FORBIDDEN);
+	}
+
+}
