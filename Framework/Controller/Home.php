64b5decc2ca429af34fea4afa911703349f11c83
Peter Gribanov <info@peter-gribanov.ru>
2013-05-20 12:24:42 +0400

    create method for user login

--- Framework/Controller/Home.php
+++ Framework/Controller/Home.php
@@ -10,8 +10,11 @@
 
 namespace Framework\Controller;
 
-
 use Framework\Controller\Controller;
+use Framework\Database\Engine;
+use Framework\Model\Users;
+use Framework\Http\Forbidden;
+
 
 /**
  * Главный контроллер
@@ -54,7 +57,14 @@ class Home extends Controller {
 	 * @return array
 	 */
 	public function usersAction() {
-		return array();
+		if (empty($_SESSION['user']) || $_SESSION['user']['role'] !== Users::ROLE_ADMIN) {
+			throw new Forbidden('Доступ к разделу запрещен');
+		}
+		$users = $this->getFactory()->getModel()->Users();
+		$users_list = $users->fetchAll();
+		return array(
+			'users' => $users_list
+		);
 	}
 
 	/**
@@ -63,6 +73,20 @@ class Home extends Controller {
 	 * @return array
 	 */
 	public function loginAction() {
-		return array();
+		if ($this->getRequest()->server('REQUEST_METHOD', 'GET') == 'POST' &&
+			($email = $this->getRequest()->post('email')) &&
+			($password = $this->getRequest()->post('password'))
+		) {
+			$users = $this->getFactory()->getModel()->Users();
+			if ($user = $users->getUserByPass($email, $password)) {
+				$_SESSION['user'] = $user;
+			} else {
+				return array('error' => 'Не верный логин или пароль');
+			}
+		}
+
+		return array(
+			'user' => !empty($_SESSION['user']) ? $_SESSION['user'] : array()
+		);
 	}
 }
