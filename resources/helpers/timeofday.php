4abca71ed91f3576f56053bcf252c4a8a763c43d
Peter Gribanov <info@peter-gribanov.ru>
2013-05-21 17:15:59 +0400

    add helper for show time of day

---
+++ resources/helpers/timeofday.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Framework package
+ *
+ * @package   Framework
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2012, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+
+/**
+ * Форматирование даты
+ *
+ * @param string  $format    Формат
+ * @param mixed   $timestamp UTM время
+ * 
+ * @return string
+ */
+return function (array $values, $timestamp = null) {
+	if ($timestamp && !is_int($timestamp)) {
+		$timestamp = strtotime($timestamp);
+	}
+	$timestamp = $timestamp ?: time();
+	// учитываем при расчете минуты
+	$hour = round((date('H', $timestamp)*3600+date('i', $timestamp)) / 3600);
+
+	$values = array('morning', 'day', 'evening', 'night') + $values;
+	if ($hour >= 4 && $hour < 12) {
+		return $values[0];
+	} elseif ($hour >= 12 && $hour < 17) {
+		return $values[1];
+	} elseif ($hour >= 17 && $hour < 24) {
+		return $values[2];
+	} else {
+		return $values[3];
+	}
+};
